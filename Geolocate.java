8274a31cb23c3f89e639e45534c7f59df3bbe48a
Eric Fay <edf4550@gmail.com>
2016-04-18 18:00:11 -0400

    fix

--- Geolocate.java
+++ Geolocate.java
@@ -6,35 +6,40 @@ import java.time.format.*;
 public class Geolocate {
 
   public static void main(String[] args) throws Exception {
-    String pattern = "yyyy:MM:dd HH:mm:ss";
     BufferedReader buffer = null;
     String line = null;
-    ArrayList<LocalDateTime> timestamps = new ArrayList<LocalDateTime>();
-    try {
-      buffer = new BufferedReader(new FileReader("timestamps.dat"));
-      while ((line=buffer.readLine()) != null) {
-        timestamps.add(LocalDateTime.parse(line, DateTimeFormatter.ofPattern(pattern)));
-      }
-    } 
-    catch(Exception e) { e.printStackTrace(); }
-    finally { if (buffer != null) { buffer.close(); } }
 
-    //ArrayList<> tuple?? location and time maybe a string +34.6273-084.1935/2015-04-01T08:04:20
-    // make a new class GeoCoordinate or find one on github
-    ArrayList<Double> latitudes = new ArrayList<Double>();
-    ArrayList<Double> longitudes = new ArrayList<Double>();
-    ArrayList<LocalDateTime> times = new ArrayList<LocalDateTime>();
+    List<Double> latitudes = new ArrayList<Double>();
+    List<Double> longitudes = new ArrayList<Double>();
+    List<LocalDateTime> times = new ArrayList<LocalDateTime>();
+    int numKnownCoordinates = 0;
     try{
       buffer = new BufferedReader(new FileReader("coords.dat"));
       while ((line=buffer.readLine()) != null) {
-        System.out.println("Latitude:"+line.substring(0,8));
-        System.out.println("Longitude:"+line.substring(8,17));
-        System.out.println("DateTime:"+line.substring(18));
+        latitudes.add( Double.parseDouble(line.substring(0,8)) );
+        longitudes.add( Double.parseDouble(line.substring(8,17)) );
+        times.add( LocalDateTime.parse(line.substring(18)) );
+        numKnownCoordinates++;
       }
     }
     catch(Exception e) { e.printStackTrace(); }
     finally { if (buffer != null) { buffer.close(); } }
-    
-//    for(LocalDateTime t:timestamps){System.out.println(t.toString());}
+
+    List<Double> LAT = Collections.unmodifiableList(latitudes);
+    List<Double> LONG = Collections.unmodifiableList(longitudes);
+    List<LocalDateTime> TIME = Collections.unmodifiableList(times);
+
+    String pattern = "yyyy:MM:dd HH:mm:ss";
+    LocalDateTime timestamp;
+    ArrayList<String> result = new ArrayList<String>();
+    try {
+      buffer = new BufferedReader(new FileReader("timestamps.dat"));
+      while ((line=buffer.readLine()) != null) {
+        timestamp = LocalDateTime.parse(line, DateTimeFormatter.ofPattern(pattern));
+        result.add("/"+timestamp.toString());
+      }
+    } 
+    catch(Exception e) { e.printStackTrace(); }
+    finally { if (buffer != null) { buffer.close(); } }
   }
 }
